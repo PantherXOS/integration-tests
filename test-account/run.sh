77c1242c99935031e73ca19593d996b8fddde934
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-07-24 17:13:43 +0430

    service start script added to runner script

--- test-account/run.sh
+++ test-account/run.sh
@@ -1,10 +1,11 @@
 # ==================================================
-DONE='\033[1;32mDONE\033[0m'
-FAIL='\033[1;31mFAIL\033[0m'
+DONE='[\033[1;32mDONE\033[0m]'
+FAIL='[\033[1;31mFAIL\033[0m]'
 
 LOG_PATH="./logs"
 GENERAL_LOG="$LOG_PATH/general.log"
 NO_DEPENDENCY=0
+DEBUG=0
 # ==================================================
 parse_input() {
     for param in $@; do
@@ -12,10 +13,24 @@ parse_input() {
         "--no-dependency")
             NO_DEPENDENCY=1
             ;;
+        "--debug")
+            DEBUG=1
+            ;;
         *)
             ;;
         esac
     done;
+}
+# ==================================================
+LOG_DBG() {
+    filename="$1"
+    data=($@)
+    data="${data[@]:1}"
+    if [ $DEBUG -eq 1 ]; then
+        echo $data | tee -a $filename
+    else
+        echo $data > $filename
+    fi
 }
 # ==================================================
 cleanup() {
@@ -34,9 +49,9 @@ install_dependency () {
     res="$(guix package -i $1 2>&1)"
     ret="$?"
     if [  $ret -eq 0 ]; then
-        printf "\t$DONE\n" | tee -a $GENERAL_LOG
+        printf "$DONE\n" | tee -a $GENERAL_LOG
     else
-        printf "\t$FAIL\n" | tee -a $GENERAL_LOG
+        printf "$FAIL\n" | tee -a $GENERAL_LOG
         echo $res | tee -a $GENERAL_LOG
     fi
     return $ret
@@ -53,12 +68,34 @@ install_dependencies () {
     done
 }
 # ==================================================
+run_service() {
+    IFS='
+    '
+    service=$1
+    echo -n "     - $1" | tee -a $GENERAL_LOG
+    for pid in $(ps x | grep -v grep | grep "$service" | awk '{print $1}'); do 
+        printf " - killing $pid";  
+        kill -9 $pid; 
+    done;
+    IFS=
+    "$service" -d > "$LOG_PATH/$service.log" 2>&1 &
+    printf " $DONE\n"
+}
+# ==================================================
+run_services() {
+    echo "> Run Required Services:"
+    services=("$@")
+    for svc in "${services[@]}"; do
+        run_service $svc
+    done
+}
+# ==================================================
 run_test () {
     filename=$(basename -- "$1")
     test_ext="${filename##*.}"
     test_name="${filename%.*}"
 
-    echo -n "     - Installing $test_name " | tee -a $GENERAL_LOG
+    echo -n "     - running $test_name " | tee -a $GENERAL_LOG
     res=""
     ret=0
     if [[ $test_ext == "sh" ]]; then 
@@ -73,18 +110,19 @@ run_test () {
     fi
 
     if [ $ret -eq 0 ]; then
-        printf "\t$DONE\n" | tee -a $GENERAL_LOG
+        printf "$DONE\n" | tee -a $GENERAL_LOG
     else 
-        printf "\t$FAIL\n" | tee -a $GENERAL_LOG
+        printf "$FAIL\n" | tee -a $GENERAL_LOG
     fi
+    LOG_DBG "$LOG_PATH/$test_name.log" $res
     return $ret
 }
 # ==================================================
 run_tests () {
     tests=("$@")
     echo "> Running Test Scripts:" | tee -a $GENERAL_LOG
-    for test in "${tests[@]}"; do 
-        run_test $test;
+    for test in "${tests[@]}"; do
+        run_test $test
     done
 }
 # ==================================================
@@ -93,21 +131,30 @@ main () {
     echo "========================================"
     
     dependencies=("python"
+                  "python-pycapnp"
                   "px-accounts-service"
+                  "px-accounts-service-plugin-etherscan"
+                  "px-accounts-service-plugin-cryptocurrency"
                   "px-events-service"
                   "px-secret-service")
+
+    services=("px-accounts-service"
+              "px-secret-service"
+              "px-events-service")
     
     tests=($(ls test-*))
+    IFS=
+    
     
     # ......................
-    cleanup
+    cleanup 
     if [ $NO_DEPENDENCY -eq 0 ]; then
         install_dependencies "${dependencies[@]}"
         if [ $? -ne 0 ]; then 
             exit
         fi
     fi
-
+    run_services "${services[@]}";
     run_tests "${tests[@]}";
 }
 # ==================================================
