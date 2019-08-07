b2dc025fd1dbd150c63069b7e68cade4ca9ca7d2
hamzeh nasajpour <h.nasajpour@pantherx.org>
2019-08-08 01:08:37 +0430

    Update `setup` test scripts based on bats-core.

---
+++ test-setup/prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+guix pull
+guix package -i px-lib-rw-guix-config px-setup-assistant
--- test-setup/test.sh
+++ test-setup/test.sh
@@ -10,16 +10,6 @@ locale=en_US.utf8
 comment="no comment"
 hostname=GUIX$user
 
-echo " + Package installation:"
-guix package -i px-lib-rw-guix-config px-setup-assistant > pkg-installation.log 2>&1
-if [ $? -eq 0 ]; then
-    echo "   - px-lib-rw-guix-config : installed"
-    echo "   - px-setup-assistant    : installed"
-else
-	echo "   - ! error in package installation (more information in pkg-installation.log)"
-	exit -1
-fi
-
 echo " + Running px-setup-assistant:"
 echo "   - Add user as an argument"
 px-setup-assistant -p desktop -u $user -g $group -t $tz -l $locale -c "$comment" -H $hostname -d debug -r true > setup-1.log 2>&1
@@ -30,6 +20,7 @@ if [ $? -eq 0 ]; then
 			echo "      * new-config.scm generated"
 			echo "      * guix reconfiguration done successfully"
 			echo "   - Done successfully"
+			exit 0
 		else
 			echo "     /home/$user not available (more information in setup-1.log)"
 		fi
