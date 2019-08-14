e3beedbe7e211b7f77eaec1100553d515ae7899e
f.sajadi <f.sajadi@pantherx.org>
2019-08-14 12:20:48 +0430

    Update backup test scripts.

--- test-backup/test-validkey.sh
+++ test-backup/test-validkey.sh
@@ -16,12 +16,13 @@ echo "     - copy tarsnap.key done"
 # run backup script
 echo "   + run px-org-remote-backup-create $user:"
 px-org-remote-backup-create.sh $user > logs/create-backup-valid.log 2>&1
-rm /home/$user/test.file
 if [ $? -eq 0 ]; then
+	rm /home/$user/test.file
 	echo "     - Backup created successfully"
 	echo "     - Log to /var/log/backup.log"
 	exit 0
 else
+	rm /home/$user/test.file
 	echo "     - Backup created failed (more information in logs/create-backup-valid.log)"
 	echo "     - Log to /var/log/backup.log"
 	exit -1
