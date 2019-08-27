e6732fadf7ba324ab5a2d9628aabc810366f57f6
Hamzeh N <h.nasajpour@pantherx.org>
2019-08-27 19:33:19 +0000

    Update test-settings-plugin-account/prerequisites.sh

--- test-settings-plugin-account/prerequisites.sh
+++ test-settings-plugin-account/prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 guix pull
-guix package -i px-settings-service px-secret-service px-events-service px-accounts-service px-accounts-service-plugin-etherscan px-settings-service-plugin-accounts-service
+guix package -i px-secret-service px-events-service px-accounts-service px-accounts-service-plugin-etherscan px-accounts-service-plugin-blockio px-settings-service px-settings-service-plugin-accounts-service 
