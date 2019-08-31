6dc94130aab0f152672d7e305acb3206d4298d04
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-09-01 00:07:05 +0430

    integrate Settings Service Plugin Account tests

--- test-settings-plugin-account/prerequisites.sh
+++ test-settings-plugin-account/prerequisites.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-guix pull
 guix package -i px-secret-service px-events-service px-accounts-service px-accounts-service-plugin-etherscan px-accounts-service-plugin-blockio px-settings-service px-settings-service-plugin-accounts
