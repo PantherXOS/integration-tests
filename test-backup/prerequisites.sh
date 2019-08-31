67a9925c3e80b1e686c58315f04a01ca5d557ce0
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-08-31 23:49:57 +0430

    update prerequisites for backup service

--- test-backup/prerequisites.sh
+++ test-backup/prerequisites.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-mkdir logs		
-# package installation
-echo " + Package installation:"
-guix pull
-guix package -i px-org-remote-backup-service> logs/pkg-installation.log 2>&1
-if [ $? -eq 0 ]; then
-    echo "   - px-org-remote-backup-service : installed"
-else
-	echo "   - ! error in package installation (more information in logs/pkg-installation.log)"
-	exit -1
-fi
+guix package -i px-org-remote-backup-service
