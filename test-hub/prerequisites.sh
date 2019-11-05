762e55eda591fca0bd1cd9011274be03c04927cc
f.sajadi <f.sajadi@pantherx.org>
2019-11-05 17:26:58 +0330

    hub integration test

---
+++ test-hub/prerequisites.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+guix pull
+guix package -i px-accounts-service px-events-service px-hub-service-plugin-python-test px-hub-service-plugin-python-test-1 px-hub-service-plugin-cpp-test
+
---
+++ test-hub/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir -p logs
+# copy sample account files
+cp test-integration-accounts.yaml ~/.userdata/accounts/
+cp plugin-cpp-test-sampl-struct.yaml ~/.userdata/hub/
+
+px-accounts-service -d &> logs/accounts.log &
+px-events-service -d &> logs/events.log &
+px-hub-service d &> logs/hub.log &
+sleep 2s
+
+bats .
+
+for pid in $(ps aux | grep -v grep | grep px-accounts-service | awk '{print $2}'); do
+    kill $pid;
+done
+for pid in $(ps aux | grep -v grep | grep px-hub-service | awk '{print $2}'); do
+    kill $pid;
+done
+for pid in $(ps aux | grep -v grep | grep px-events-service | awk '{print $2}'); do
+    kill $pid;
+done
+rm ~/.userdata/accounts/test-integration-accounts.yaml 
